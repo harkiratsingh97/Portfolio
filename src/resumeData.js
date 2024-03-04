7ea8cf39b1ac196c7f5eb28ded25d6c3a44f8401
harkiratsinghh <singh.harkirat97@gmail.com>
2024-03-04 18:34:53 +0530

    portfolio made

---
+++ src/resumeData.js
@@ -0,0 +1,121 @@
+export const resumeDataBack = {
+	main: {
+		name: "Harkirat Singh",
+		occupation: "Full Stack Developer",
+		description: "Full Stack Developer by Day, Founder && Freelancer by Night.",
+		image: "IG_Harkirat.png",
+		bio: "My name is Harkirat Singh, I'm a Full Stack Developer. Currently I'm working at InternNexus. ",
+		contactmessage: "Get in touch with me to receive further details.",
+		email: "officialharkiratsingh@gmail.com",
+		phone: "+91 8882119791",
+		address: {
+			street: "B-62, Shankar Garden",
+			city: "New Delhi",
+			state: "Delhi",
+			zip: "110018",
+		},
+
+		social: [
+			{
+				name: "facebook",
+				url: "https://www.facebook.com/harkirat.singh.94",
+				className: "fa fa-facebook",
+			},
+			{
+				name: "linkedin",
+				url: "https://www.linkedin.com/in/harkirat-singh-ba205b184/",
+				className: "fa fa-linkedin",
+			},
+			{
+				name: "instagram",
+				url: "https://www.instagram.com/harkiratsingh_hs/",
+				className: "fa fa-instagram",
+			},
+			{
+				name: "github",
+				url: "http://github.com/harkiratsingh97",
+				className: "fa fa-github",
+			},
+		],
+	},
+	resume: {
+		skillmessage:
+			"Here you can create a short write-up of your skills to show off to employers",
+		education: [
+			{
+				school: "Guru Gobind Singh Indraprastha University",
+				degree: "Bachelor of Technology in Computer Science",
+				graduated: "September 2020",
+				description: "Graduated with Distinction",
+			},
+		],
+		work: [
+			{
+				company: "Infosys",
+				title: "Full Stack Developer",
+				years: "Jan 2021 - Present",
+				description: "Developing websites as Full Stack Developer.",
+			},
+			{
+				company: "Intern Nexus",
+				title: "Intern as Full Stack Developer",
+				years: "June 2020 - Present",
+				description: "Developing websites as Full Stack Developer.",
+			},
+		],
+		skills: [
+			{
+				name: "AngularJS",
+				level: "90%",
+			},
+			{
+				name: "CSS",
+				level: "95%",
+			},
+			{
+				name: "Git",
+				level: "60%",
+			},
+			{
+				name: "NodeJS",
+				level: "85%",
+			},
+			{
+				name: "HTML5",
+				level: "80%",
+			},
+			{
+				name: "MongoDB",
+				level: "80%",
+			},
+			{
+				name: "ReactJS",
+				level: "50%",
+			},
+		],
+	},
+	portfolio: {
+		projects: [
+			{
+				title: "SM Crane",
+				category: "A website for a crane company (AngularJS)",
+				image: "Untitled.png",
+				url: "http://www.smcranesolution.com",
+			},
+			{
+				title: "hg crane",
+				category: "A djsdjndsjkcjkdnc",
+				image: "Untitled.png",
+				url: "http://wwwsbsdsjbn",
+			},
+		],
+	},
+	testimonials: {
+		testimonials: [
+			{
+				text: "Here you should write some nice things that someone has said about you. No lies though, employers can tell when you are lying.",
+				user: "Kareem Abdul Jabbar",
+			},
+		],
+	},
+};
