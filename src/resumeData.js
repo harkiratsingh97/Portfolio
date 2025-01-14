f0c512eb1c082c424b2c0a76dc7e9a49d9f9a7af
Harkirat Singh <officialharkiratsingh@gmail.com>
2025-01-14 09:53:17 +0530

    Change resume data

--- src/resumeData.js
+++ src/resumeData.js
@@ -51,9 +51,16 @@ export const resumeDataBack = {
 		],
 		work: [
 			{
+				company: "BOLD Technologies",
+				title: "Software Engineer",
+				years: "Jul 2024 - Present",
+				description:
+					"Spearheaded multiple Single Page Application (SPA) experiments aimed at boosting user engagement through A/B testing, resulting in a 20% increase in user interaction. Led the React.js version upgrade for several key modules, improving performance and reducing codebase complexity by 15%. Integrated Google Drive for seamless file uploads, enabling users to upload files 30% faster, enhancing overall user experience. Implemented Localization for multiple countries, expanding the product’s reach and supporting 7+ new languages, resulting in a 25% growth in international users. Collaborated across cross-functional teams to maintain and enhance shared code repositories, enabling smoother collaboration and faster development cycles. Conducted config-based development to provide flexible feature deployment across various environments, reducing setup time by 40%. Optimized web page load times by implementing Next.js server-side rendering (SSR), resulting in a 35% reduction in average user wait times and a 20% improvement in SEO rankings. This performance boost has since become the standard for all future projects.",
+			},
+			{
 				company: "Infosys",
 				title: "Technology Analyst",
-				years: "Jan 2021 - Present",
+				years: "Jan 2021 - June 2024",
 				description:
 					"As a Technology Analyst at Infosys, I apply expertise in Full Stack Development and Data Structures and Algorithms (DSA) to create innovative web applications and solve complex problems. With over 3 years of hands-on experience in the IT industry, I have delivered robust and efficient solutions for various clients and projects, using JavaScript as my primary programming languages.",
 			},
