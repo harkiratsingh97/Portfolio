5f4bcfee3450c93c8a87e77fa8562f8f714dc6ae
harkiratsingh <officialharkiratsingh@gmail.com>
2026-06-14 23:10:21 +0530

    update job

--- src/App.js
+++ src/App.js
@@ -11,28 +11,28 @@ import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
 
 function App() {
-	const [resumeData, setResumeData] = useState(null);
+  const [resumeData, setResumeData] = useState(null);
 
-	// console.log(resumeData)
-	useEffect(() => {
-		setResumeData(resumeDataBack);
-	}, []);
+  // console.log(resumeData)
+  useEffect(() => {
+    setResumeData(resumeDataBack);
+  }, []);
 
-	return (
-		<div className="App">
-			{resumeData && (
-				<>
-					<Header data={resumeData.main} />
-					<About data={resumeData.main} />
-					<Resume data={resumeData.resume} />
-					<Portfolio data={resumeData.portfolio} />
-					{/* <Testimonials data={resumeData.testimonials}/> */}
-					<Contact data={resumeData.main} />
-					<Footer data={resumeData.main} />
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className="App">
+      {resumeData && (
+        <>
+          <Header data={resumeData.main} />
+          <About data={resumeData.main} />
+          <Resume data={resumeData.resume} />
+          <Portfolio data={resumeData.portfolio} />
+          {/* <Testimonials data={resumeData.testimonials}/> */}
+          <Contact data={resumeData.main} />
+          <Footer data={resumeData.main} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,144 +1,155 @@
 export const resumeDataBack = {
-	main: {
-		name: "Harkirat Singh",
-		occupation: "Full Stack Developer",
-		description:
-			"Software Engineer at BOLD | Computer Science, Data Structures, Node.js, React.js, Next.js",
-		image: "IG_Harkirat.jpg",
-		bio: "Full Stack Developer with Expertise in Node.js, Next.js, React.js, Tailwind CSS, JavaScript, Redux, Agile Methodology, and Database Management.",
-		contactmessage: "Get in touch with me to receive further details.",
-		email: "officialharkiratsingh@gmail.com",
-		phone: "+91 8920923903",
-		address: {
-			street: "Shankar Garden",
-			city: "New Delhi",
-			state: "Delhi",
-			zip: "110018",
-		},
-		resumedownload: "Harkirat_Resume.pdf",
+  main: {
+    name: "Harkirat Singh",
+    occupation: "Full Stack Developer",
+    description:
+      "Software Engineer at Sprinklr | Computer Science, Data Structures, Node.js, React.js, Next.js",
+    image: "IG_Harkirat.jpg",
+    bio: "Full Stack Developer with Expertise in Node.js, Next.js, React.js, Tailwind CSS, JavaScript, Redux, Agile Methodology, GraphQL, and Database Management.",
+    contactmessage: "Get in touch with me to receive further details.",
+    email: "officialharkiratsingh@gmail.com",
+    phone: "+91 8920923903",
+    address: {
+      street: "Shankar Garden",
+      city: "New Delhi",
+      state: "Delhi",
+      zip: "110018",
+    },
+    resumedownload: "Harkirat_Resume.pdf",
 
-		social: [
-			{
-				name: "facebook",
-				url: "https://www.facebook.com/harkirat.singh.94",
-				className: "fa fa-facebook",
-			},
-			{
-				name: "linkedin",
-				url: "https://www.linkedin.com/in/harkiratsingh97",
-				className: "fa fa-linkedin",
-			},
-			{
-				name: "instagram",
-				url: "https://www.instagram.com/harkiratsingh_hs/",
-				className: "fa fa-instagram",
-			},
-			{
-				name: "github",
-				url: "http://github.com/harkiratsingh97",
-				className: "fa fa-github",
-			},
-		],
-	},
-	resume: {
-		education: [
-			{
-				school: "Guru Gobind Singh Indraprastha University",
-				degree: "Bachelor of Technology in Computer Science",
-				graduated: "September 2020",
-				description: "Graduated with Distinction",
-			},
-		],
-		work: [
-			{
-				company: "BOLD Technologies",
-				title: "Software Engineer",
-				years: "Jul 2024 - Present",
-				description:
-					"Spearheaded multiple SPA experiments, achieving a 20% increase in user engagement. Upgraded React.js for key modules, reducing code complexity by 15% and enhancing performance. Integrated Google Drive for 30% faster file uploads and implemented Localization, supporting 7+ languages and driving 25% growth in international users. Streamlined collaboration by maintaining shared code repositories and enabled flexible feature deployment, cutting setup time by 40%. Optimized page load times with Next.js SSR, reducing wait times by 35% and improving SEO by 20%, setting a benchmark for future projects.",
-			},
-			{
-				company: "Infosys",
-				title: "Technology Analyst",
-				years: "Jan 2021 - June 2024",
-				description:
-					"Orchestrated agile collaboration across 3 projects, boosting efficiency by 20%. Revamped internal tool interfaces using React.js, Redux, and Flux, reducing error rates by 30% and increasing user satisfaction by 25%. Built scalable APIs with Node.js and Express.js, enhancing system performance by 40%. Deployed survey forms for Daimler, driving a 50% improvement in employee performance through actionable insights. Leveraged expertise in React and Node.js to redesign user interfaces, increasing engagement and retention by 40%.",
-			}
-		],
+    social: [
+      {
+        name: "facebook",
+        url: "https://www.facebook.com/harkirat.singh.94",
+        className: "fa fa-facebook",
+      },
+      {
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/harkiratsingh97",
+        className: "fa fa-linkedin",
+      },
+      {
+        name: "instagram",
+        url: "https://www.instagram.com/harkiratsingh_hs/",
+        className: "fa fa-instagram",
+      },
+      {
+        name: "github",
+        url: "http://github.com/harkiratsingh97",
+        className: "fa fa-github",
+      },
+    ],
+  },
+  resume: {
+    education: [
+      {
+        school: "Guru Gobind Singh Indraprastha University",
+        degree: "Bachelor of Technology in Computer Science",
+        graduated: "September 2020",
+        description: "Graduated with Distinction",
+      },
+    ],
+    work: [
+      {
+        company: "Sprinklr",
+        title: "Senior Software Engineer",
+        years: "Jun 2025 - Present",
+        description:
+          "Spearheaded multiple SPA experiments, achieving a 20% increase in user engagement. Upgraded React.js for key modules, reducing code complexity by 15% and enhancing performance. Integrated Google Drive for 30% faster file uploads and implemented Localization, supporting 7+ languages and driving 25% growth in international users. Streamlined collaboration by maintaining shared code repositories and enabled flexible feature deployment, cutting setup time by 40%. Optimized page load times with Next.js SSR, reducing wait times by 35% and improving SEO by 20%, setting a benchmark for future projects.",
+      },
+      {
+        company: "BOLD Technologies",
+        title: "Software Engineer",
+        years: "Jul 2024 - Jun 2025",
+        description:
+          "Spearheaded multiple SPA experiments, achieving a 20% increase in user engagement. Upgraded React.js for key modules, reducing code complexity by 15% and enhancing performance. Integrated Google Drive for 30% faster file uploads and implemented Localization, supporting 7+ languages and driving 25% growth in international users. Streamlined collaboration by maintaining shared code repositories and enabled flexible feature deployment, cutting setup time by 40%. Optimized page load times with Next.js SSR, reducing wait times by 35% and improving SEO by 20%, setting a benchmark for future projects.",
+      },
+      {
+        company: "Infosys",
+        title: "Technology Analyst",
+        years: "Jan 2021 - June 2024",
+        description:
+          "Orchestrated agile collaboration across 3 projects, boosting efficiency by 20%. Revamped internal tool interfaces using React.js, Redux, and Flux, reducing error rates by 30% and increasing user satisfaction by 25%. Built scalable APIs with Node.js and Express.js, enhancing system performance by 40%. Deployed survey forms for Daimler, driving a 50% improvement in employee performance through actionable insights. Leveraged expertise in React and Node.js to redesign user interfaces, increasing engagement and retention by 40%.",
+      },
+    ],
 
-		skillmessage:
-			"Skilled in developing modern web applications with React.js and Node.js, leveraging JavaScript for clean, efficient code. Proficient in MongoDB for scalable data management, with a strong foundation in essential data structures",
+    skillmessage:
+      "Skilled in developing modern web applications with React.js and Node.js, leveraging JavaScript for clean, efficient code. Proficient in MongoDB for scalable data management, with a strong foundation in essential data structures",
 
-		skills: [
-			{
-				name: "ReactJS",
-				level: "100%",
-			},
-			{
-				name: "Redux.js",
-				level: "90%",
-			},
-			{
-				name: "JavaScript",
-				level: "100%",
-			},
-			{
-				name: "NodeJS",
-				level: "78%",
-			},
-			{
-				name: "Next.js",
-				level: "70%",
-			},
-			{
-				name: "SASS / CSS",
-				level: "75%",
-			},
-			{
-				name: "Git",
-				level: "100%",
-			},
-			{
-				name: "Github",
-				level: "80%",
-			},
-			{
-				name: "JIRA",
-				level: "90%",
-			},
-			{
-				name: "SQL",
-				level: "80%",
-			},
-			{
-				name: "MongoDB",
-				level: "80%",
-			},
-		],
-	},
-	portfolio: {
-		projects: [
-			{
-				title: "Kirat Estate",
-				category: "A real Estate website for connecting landlord/tenants",
-				image: "KiratEstate.png",
-				url: "https://real-estate-app-69v9.onrender.com/",
-			},
-			{
-				title: "Busy Buy",
-				category:
-					"An E-Commerce website (React.js and Firebase/Firestore Authentication)",
-				image: "busy-buy.png",
-				url: "https://busybuy-react.web.app/",
-			},
-		],
-	},
-	testimonials: {
-		testimonials: [
-			{
-				text: "",
-				user: "",
-			},
-		],
-	},
+    skills: [
+      {
+        name: "ReactJS",
+        level: "100%",
+      },
+      {
+        name: "Redux.js",
+        level: "90%",
+      },
+      {
+        name: "GraphQL",
+        level: "70%",
+      },
+      {
+        name: "JavaScript",
+        level: "100%",
+      },
+      {
+        name: "NodeJS",
+        level: "78%",
+      },
+      {
+        name: "Next.js",
+        level: "70%",
+      },
+      {
+        name: "SASS / CSS",
+        level: "75%",
+      },
+      {
+        name: "Git",
+        level: "100%",
+      },
+      {
+        name: "Github",
+        level: "80%",
+      },
+      {
+        name: "JIRA",
+        level: "90%",
+      },
+      {
+        name: "SQL",
+        level: "80%",
+      },
+      {
+        name: "MongoDB",
+        level: "80%",
+      },
+    ],
+  },
+  portfolio: {
+    projects: [
+      {
+        title: "Kirat Estate",
+        category: "A real Estate website for connecting landlord/tenants",
+        image: "KiratEstate.png",
+        url: "https://real-estate-app-69v9.onrender.com/",
+      },
+      {
+        title: "Busy Buy",
+        category:
+          "An E-Commerce website (React.js and Firebase/Firestore Authentication)",
+        image: "busy-buy.png",
+        url: "https://busybuy-react.web.app/",
+      },
+    ],
+  },
+  testimonials: {
+    testimonials: [
+      {
+        text: "",
+        user: "",
+      },
+    ],
+  },
 };
